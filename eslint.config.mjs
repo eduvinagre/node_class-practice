c0a18e736feaca450113bc88ad364812f85242f5
eduvinagre <eduvinagre@gmail.com>
2024-06-26 20:46:25 -0300

    add prettier and google configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import config from "eslint-config-google";
+
 
 export default [
+  ...[].concat(config),
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
